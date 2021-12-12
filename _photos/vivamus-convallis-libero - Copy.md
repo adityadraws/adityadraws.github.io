d76a41377dd17bfda28c84fb1800f5cf175abee7
Aditya Ramesh <ramesh.aditya@gmail.com>
2021-12-12 23:14:15 +0530

    Build gallery, add builder script

--- _photos/vivamus-convallis-libero - Copy.md
+++
@@ -1,6 +0,0 @@
----
-title: here
-image: assets/images/fulls/here.png
-thumbnail: assets/images/thumbs/here.png
-caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
----
--- build_gallery.py
+++ build_gallery.py
@@ -26,6 +26,40 @@ dated_folders = os.walk(ROOT)
 
 dry_run = False
 
+im = r'C:\Users\gayat\Desktop\Instagram data backups\adityadraws_20211212\media\posts\202106\200509058_3047549632142829_5999638296946235673_n_18135034312168803.jpg'
+
+def resize(image):
+    from PIL import Image, ImageOps
+    from math import ceil
+    img = Image.open(image)
+    print(img.width, img.height)
+
+    # If it is not 4:3, make it 4:3 ratio first by adding borders
+    # then resize to 1200:750
+
+    w = img.width
+    h = img.height
+    new_h = h
+    new_w = w
+    if w > h:
+        new_h = 3/4 * w
+    if w <= h:
+        new_w = 4/3 * h
+
+    horizontal_border = new_w - w
+    vertical_border = new_h - h
+
+    up = ceil(vertical_border/2)
+    down = ceil(vertical_border/2)
+    left = ceil(horizontal_border/2)
+    right = ceil(horizontal_border/2)
+    print(left, up, right, down)
+    #new_img = ImageOps.expand(img, (up, right, down, left), fill = 'black')
+    new_img = ImageOps.expand(img, (left, up, right, down), fill = 'black')
+    size = (1200, 800)
+    new_img.save(image)
+
+
 for folder in dated_folders:
     path_to_folder = folder[0]
 
@@ -50,9 +84,10 @@ for folder in dated_folders:
         output_image_title = time_of_posting + ' Art#' + str(post_num)
         output_filename    = time_of_posting + '_' + str(post_num)
 
-        destination_thumbnail = THUMB_ROOT + '\\' + output_filename
-        destination_full      = FULL_ROOT + '\\' + output_filename
+        destination_thumbnail = THUMB_ROOT + '\\' + output_filename + '.jpg'
+        destination_full      = FULL_ROOT + '\\' + output_filename + '.jpg'
         source_image          = path_to_folder + '\\' + image_filename
+        resize(source_image)
 
         print('Copying', source_image, 'to', destination_thumbnail)
         print('Copying', source_image, 'to', destination_full)
@@ -70,7 +105,8 @@ for folder in dated_folders:
         content_image_path = 'assets/images/fulls/' + output_filename + '.jpg'
         content_thumbnail_path = 'assets/images/thumbs/' + output_filename + '.jpg'
         content = POST_CONTENT.format(output_image_title, content_image_path, content_thumbnail_path, '')
-
+        new_post.write(content)
+        
         if not dry_run:
             new_post.close()
             
